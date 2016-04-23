9f26dd11419edebb544bf4d6ef5e3025b6bdbe94
Erwin Jansen <pokowaka@gmail.com>
2016-04-23 00:56:40 -0700

    A stack that tracks the minimum value in the stack.
    - Runs in O(1)
    - Space O(1)

--- lib/puzzles.rb
+++ lib/puzzles.rb
@@ -1,4 +1,5 @@
 require 'puzzles/version'
+require 'puzzles/minstack'
 
 module Puzzles
   # Your code goes here...
@@ -7,6 +8,7 @@ module Puzzles
     prng = Random.new
     iterator.each_with_index do |elem, idx|
       # Replace an element in the list
+      # Chance per idx = 1/1, 1/2, 1/3, 1/4, 1/5, .... 1/n
       (0..k - 1).each { |x| result[x] = elem if prng.rand < (1.0 / (idx + 1)) }
     end
     result
